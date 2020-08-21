109083cee90da81f616ee8b6f0a007297d1b4bf5
amir <inbox_me@hotmail.com>
2020-08-21 15:32:24 +0545

    cssfiles

---
+++ views/php/head.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo '
+<link href="https://fonts.googleapis.com/css?family=Ubuntu:700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="views/css/style.css">
+<link rel="icon" href="views/images/logo.png">
+<title>Online Bazzar</title>
+';
+
+?>
